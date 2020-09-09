5004cae0b43d7a3e9c9c6bb173dd744328bf7c91
clintchu <clintchu@outlook.com>
2020-09-08 20:58:35 -0700

    Latest version

--- dist/bundle.js
+++ dist/bundle.js
@@ -94,27 +94,21 @@
 /***/ (function(module, exports) {
 
 const canvas = document.querySelector("canvas")
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth / 1.5
+canvas.height = window.innerHeight / 1.5
 const ctx = canvas.getContext("2d")
 
 // Create Piggy
 const PiggyBankers = () => {
     ctx.fillStyle = "rgba(255, 0, 0, 0.5)"
-    ctx.fillRect(100, 100, 125, 125)
-}
-
-// Create Item
-const createItem = () => {
-    ctx.fillStyle = "rgba(0, 255, 0, 0.5)"
-    ctx.fillRect(300, 300, 50, 50)
+    ctx.fillRect((canvas.width-225) / 1.5, (canvas.height-225) / 1.5, 100, 100);
 }
 
 // Create Gold Coins
 const createCoin = () => {
 for (let i = 0; i < 4; i++) {
-    const x = Math.random() * window.innerWidth
-    const y = Math.random() * window.innerHeight
+    const x = Math.random() * window.innerWidth / 1.5
+    const y = Math.random() * window.innerHeight / 1.5
 
     ctx.beginPath()
     ctx.arc(x, y, 30, 0, Math.PI * 2, false)
@@ -122,57 +116,47 @@ for (let i = 0; i < 4; i++) {
     ctx.fill()
 }}
 
+// Create Shopping Item
+const createShoppingItem = () => {
+    ctx.fillStyle = "rgba(0, 255, 0, 0.5)"
+    ctx.fillRect(125, 125, 50, 50)
+}
 
 // Pause Game
 const showPaused = () => {
     ctx.textAlign = "center"
-    ctx.font = "35px Arial"
+    ctx.font = "20px Arial"
     ctx.fillStyle = "black"
-    ctx.fillText("PAUSED", width / 2, height / 2)
+    ctx.fillText("PAUSED", canvas.width / 2, canvas.height / 2)
 }
 
 // Show Score
 const showScore = () => {
     ctx.textAlign = "center"
-    ctx.font = "25px Arial"
+    ctx.font = "15px Arial"
     ctx.fillStyle = "black"
-    ctx.fillText("SCORE: " + score, width-120, 30)
+    ctx.fillText("SCORE: " + score, canvas.width-90, 50)
 }
 
-// Refresh Game
-const refresh = () => {
-    if (isPaused) {
-        return
-    }
-
-    // If Paused Prevent Play
-
-    // If click item, remove life
-
-    // If deposit money, add score
-
-    showScore()
-}
-
+// Initialize Game
 const game = () => {
-    const tileSize = 20
-
-    width = tileSize * Math.floor(window.innerWidth / tileSize)
-    height = tileSize * Math.floor(window.innerHeight / tileSize)
+    
+    width = Math.floor(window.innerWidth)
+    height = Math.floor(window.innerHeight)
 
     isPaused = false
     score = 0
 
-    // createItem()
+    showScore()
     PiggyBankers()
-    refresh()
     createCoin()
+    createShoppingItem()
+
 }
 
 window.addEventListener("keydown", (event) => {
     if (event.key === " ") {
         event.preventDefault()
-        console.log("paused")
         isPaused = !isPaused
         showPaused()
     }
@@ -188,17 +172,20 @@ window.addEventListener("load", () => {
     // Scale -- this ctx.scale(x, y);
     // Rotate -- this ctx.rotate(20 * Math.PI / 180);
 
-    
+
 let isDragging = false
 let x = 0
 let y = 0
 
-const myCoin = document.getElementById("coin")
+const myCoin = document.getElementById("coin");
 
 myCoin.addEventListener("mousedown", e => {
     x = e.offsetX
     y = e.offsetY
     isDragging = true
+
+    ctx.fillStyle = "rgba(0, 255, 0, 0.5)"
+    ctx.fillRect(x, y, 50, 50)
 })
 
 myCoin.addEventListener("mousemove", e => {
@@ -206,6 +193,7 @@ myCoin.addEventListener("mousemove", e => {
         dragCoin(ctx, x, y, e.offsetX, e.offsetY)
         x = e.offsetX
         y = e.offsetY
+        console.log("test");
     }
 })
 
---
+++ unused.js
@@ -0,0 +1,5 @@
+// // Create Shopping Item
+// const createShoppingItem = () => {
+//     ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
+//     ctx.fillRect(125, 125, 50, 50);
+// };
