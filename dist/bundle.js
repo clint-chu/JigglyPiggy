f110434672588e17516c781d7d868de4ed5a518d
clintchu <clintchu@outlook.com>
2020-09-10 01:14:25 -0700

    Spawn coins in random places

--- dist/bundle.js
+++ dist/bundle.js
@@ -93,22 +93,47 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+// const CONSTANTS = {
+//     piggyBankersWidth = 200,
+//     piggyBankersLength = 200
+// }
+
+// const rangeX = (y) => {
+//     if (y < 200 || y > (y-200)) {
+//         x = window.innerWidth
+//     } else {
+//         if (x < (x/2 - 100)) {
+//             return x = Math.floor(Math.random() * Math.floor(x/2 - 100));
+//         } else {
+//             return x = Math.floor(Math.random() * Math.floor(x/2 + 100));
+//             // This isn't right
+//         }
+//     }
+// }
+
+// const rangeY = (x) => {
+//     if (x < 200 || x > (x-200)) {
+//         y = window.innerWidth
+//     } else {
+//         if (y < (y/2 - 100)) {
+//             return y = Math.floor(Math.random() * Math.floor(y/2 - 100));
+//         } else {
+//             return y = Math.floor(Math.random() * Math.floor(y/2 + 100));
+//             // This isn't right
+//         }
+//     }
+// }
+
 const coin = document.getElementById("coin")
 const makeCoin = () => {
     const x = Math.random() * (window.innerWidth)
     const y = Math.random() * (window.innerHeight)
     const newCoin = coin.cloneNode(true)
     newCoin.removeAttribute("id")
-    newCoin.style.left = x
-    console.log("x1", x);
-    console.log("nc-before", newCoin);
-    console.log("nc-before-style", newCoin.style);
-    console.log("nc-before-style-left", newCoin.style.left);
-    newCoin.style.top = y
+    newCoin.classList.add("coin")
+    newCoin.style.left = x + "px"
+    newCoin.style.top = y + "px"
     document.getElementById("root").appendChild(newCoin)
-    console.log("x2", x);
-    console.log("nc-after", newCoin)
-    console.log("nc-after-style", newCoin.style.left)
 }
 
 const makeCoins = (n) => {
@@ -127,34 +152,39 @@ const jigglePiggyBankers = () => {
 
 const bindEvents = () => {
     document.addEventListener("click", jigglePiggyBankers)
+
+    const draggables = document.querySelectorAll(".draggable")
+    const containers = document.querySelectorAll(".container")
+    
+    draggables.forEach(draggable => {
+        draggable.addEventListener("dragstart", () => {
+            // console.log("dragstart")
+            draggable.classList.add("dragging")
+        })
+    
+        draggable.addEventListener("dragend", () => {
+            // console.log("dragend")
+            draggable.classList.remove("dragging")
+        })
+    })
+    
+    containers.forEach(container => {
+        container.addEventListener("dragover", () => {
+            const draggable = document.querySelector(".dragging")
+            // draggable.style.display = "none"
+        //   console.log("end", draggable.style.display);
+            document.getElementById("root").remove(draggable)
+        })
+    })
 }
 
 const init = () => {
-    makeCoins(1)
+    makeCoins(5)
     bindEvents()
 }
 
 init()
 
-const draggables = document.querySelectorAll(".draggable")
-const containers = document.querySelectorAll(".container")
-
-draggables.forEach(draggable => {
-    draggable.addEventListener("dragstart", () => {
-        draggable.classList.add("dragging")
-    })
-
-    draggable.addEventListener("dragend", () => {
-        draggable.classList.remove("dragging")
-    })
-})
-
-containers.forEach(container => {
-    container.addEventListener("dragover", () => {
-        const draggable = document.querySelector(".dragging")
-        container.appendChild(draggable)
-    })
-})
 
 /***/ })
 
